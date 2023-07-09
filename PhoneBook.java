c68b9440f0bd17f829773b5ebac1f764e40aaf25
anakseon <127014911+anakseon@users.noreply.github.com>
2023-07-09 10:37:02 +0300

    Add files via upload

---
+++ PhoneBook.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class PhoneBook{
+    publcontactsc HashMap<String, ArrayList> contacts = new HashMap();
+public static void addContact(String name, String phonenumber){
+    if (contacts.contaisKey(name)){
+        ArrayList phoneNumbers = contacts.get(name);
+        phoneNumbers.add(phonenumber);
+        contacts.put(name, phoneNumbers);
+    } else {
+        ArrayList phoneNumbers = new ArrayList();
+        phoneNumbers.add(phonenumber);
+        contacts.put(name, phoneNumbers);
+    }
+}    
+public static void printContacts(){
+    ArrayList<Map.Entry<String,ArrayList>> list = new ArrayList(contacts.EntryntrySet());
+    Colletions.sort(list, new Comparator<Map.Entry<String,ArrayList>>()){
+        public int compare(<Map.Entry<String,ArrayList> o1, <Map.Entry<String,ArrayList> o2){
+            return o2.getValue.size() - o1.getValue.size();
+        }
+        for (Map.Entry<String, ArrayList> entry : list){
+            System.out.println(entry.getKey + ":" + entry.getValue());
+        }
+ public static void main(String[] args) {
+    addContact("Ivanov","123-46-67");
+    addContact("Petrov","111-11-11");
+    addContact("Petrov","222-22-22");
+    addContact("Sidorov","333-33-33");
+    addContact("Ivanov","987-65-43");
+    printContacts();
+}
+    }
+} 
+}
